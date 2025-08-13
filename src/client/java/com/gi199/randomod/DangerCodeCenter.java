ff857996416110abd74187c7ccd62df4e0269ee0
gi199 <looksmc@outlook.com>
2025-08-13 22:12:28 +0800

    Remove Easter Eggs,Change Code Style

--- src/client/java/com/gi199/randomod/DangerCodeCenter.java
+++
@@ -1,5 +0,0 @@
-package com.gi199.randomod;
-
-public class DangerCodeCenter {
-    //public Object LLLLLL(RuntimeException exception){return exception;}
-}
--- src/client/java/com/gi199/randomod/Poucases.java
+++
@@ -1,9 +0,0 @@
-package com.gi199.randomod;
-
-public class Poucases {
-    public Poucases(RuntimeException exception){
-        System.out.print("Exception:"+exception);
-        throw exception;
-        new Poucases(exception);
-    }
-}
